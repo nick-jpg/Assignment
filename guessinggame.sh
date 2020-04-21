b13265531a0277bbd12784d3bb8a1bb777875936
nick-jpg <analmonster3@gmail.com>
2020-04-21 12:27:07 +0200

    Done

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,16 +1,17 @@
 function part {
-
+n=$(ls -l | wc -l)
+n2=n-1
 echo "How many files are in the current directory?"
 }
 part
-while [[ p -ne 3 ]]
+while [[ p -ne n2 ]]
 do
 
 	read p
-	if [[ $p -gt 3 ]]
+	if [[ $p -gt n2 ]]
 	then
 		echo "your guess is too high, try again!"
-	elif [[ $p -lt 3 ]]
+	elif [[ $p -lt n2 ]]
 	then
 		echo "your guess is too low, try again!"
 	else
